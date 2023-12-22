15b995493cee24cddc796a61bfde8d877addc7af
Purujit Vij <130056678+purujitv@users.noreply.github.com>
2023-12-22 18:37:55 +0530

    UI CHANGES and Button fixes

--- public/javascripts/balance.js
+++ public/javascripts/balance.js
@@ -12,33 +12,42 @@ function fetchBalance() {
     .then((data) => {
       console.log(data);
       // Update the DOM with the fetched balance
-      document.getElementById(
-        "balance-container"
-      ).innerText = `${data.data.balance}`;
-      document.getElementById(
-        "currency-container"
-      ).innerText = `Available ${data.data.currency} Balance`;
+      document.getElementById("balance-container").innerText = `${data.data.balance}`;
+      document.getElementById("currency-container").innerText = `Available ${data.data.currency} Balance`;
 
-      // Call the function to update the lobby button and show alert
-      updateLobbyButtonVisibility(data.data.balance);
+      // Call the function to update the UI based on balance
+      updateUI(data.data.balance);
     })
     .catch((error) => {
       console.error("Error during fetch:", error);
     });
 }
 
-// Function to update the lobby button and show alert if balance is insufficient
-function updateLobbyButtonVisibility(balance) {
+// Function to update the UI based on balance
+function updateUI(balance) {
   var lobbyButton = document.getElementById("lobbybtn");
+  var inputField = document.getElementById("yourInputField"); // Replace with the actual ID of your input field
+  var errorContainer = document.getElementById("error-message");
 
   // Check if the balance is less than 10.
   if (balance < 10) {
     lobbyButton.disabled = true; // Disable the button.
-    alert(
-      "Insufficient balance to play the game. Please recharge your account."
-    );
+
+    // Show error message below the input field
+    if (!errorContainer) {
+      errorContainer = document.createElement("div");
+      errorContainer.id = "error-message";
+      errorContainer.style.color = "red";
+      inputField.parentNode.insertBefore(errorContainer, inputField.nextSibling);
+    }
+    errorContainer.innerText = "Insufficient balance to play the game. Please recharge your account.";
   } else {
     lobbyButton.disabled = false; // Enable the button.
+
+    // Remove the error message
+    if (errorContainer) {
+      errorContainer.parentNode.removeChild(errorContainer);
+    }
   }
 }
 
