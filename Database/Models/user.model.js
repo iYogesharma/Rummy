c4d27923b15aeb94453b8186e9a543d8f8d5fe53
Yogesh Sharma <44565761+iYogesharma@users.noreply.github.com>
2024-07-03 12:03:34 +0530

    Update user.model.js
    Removed commented code

--- Database/Models/user.model.js
+++ Database/Models/user.model.js
@@ -1,17 +1,6 @@
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
-  // name: { type: String,  default: null },
-  // email: { type: String, unique: true },
-  // email_validated: { type: Boolean, default: false },
-  // password: { type: String,  default: null },
-  // phoneNumber: { type: Number, default: null},
-  // provider: { type: String, default: null },
-  // providerId: { type: String,  unique: true, default: null },
-  // accessToken: { type: String,  default: null },
-  // refreshToken: { type: String,  default: null },
-  // tokenExpiry: { type: Date, default: null},
-  // lightningAddress: { type: String, default: null},
   lnId:{ type: String, unique: true },
   balance: { type: Number, default: 1000},
   active: {type: Boolean,default: true},
